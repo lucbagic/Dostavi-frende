48407ebf899224952de34f8af1ec5631bb857627
Lucija-PC\Lucija Bagić <lucbagic@foi.hr>
2018-12-17 14:44:15 +0100

    Registracija pomoću Firebase-a uspješno odrađena.

--- Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/MainActivity.java
+++ Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/MainActivity.java
@@ -1,7 +1,9 @@
 package hr.dostavifrende.dostavifrende;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openRegister(View v){
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+    }
+    public void openLogin(View v){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
---
+++ Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/RegisterActivity.java
@@ -0,0 +1,56 @@
+package hr.dostavifrende.dostavifrende;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText editTextEmail;
+    EditText editTextLozinka;
+
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextLozinka = findViewById(R.id.editTextLozinka);
+
+        auth = FirebaseAuth.getInstance();
+
+    }
+
+    public void createUser(View v){
+        if(editTextEmail.getText().toString().equals("") && editTextLozinka.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(), "Niste ispunili sva polja", Toast.LENGTH_SHORT).show();
+        }else {
+            final String email = editTextEmail.getText().toString();
+            final String loz = editTextLozinka.getText().toString();
+
+            auth.createUserWithEmailAndPassword(email, loz)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Toast.makeText(getApplicationContext(), "Uspješno ste se registrirali", Toast.LENGTH_SHORT).show();
+                            }else {
+                                Log.i("log",loz+email);
+                                Toast.makeText(getApplicationContext(), "Korisnik nije kreiran", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
+        }
+    }
+}
