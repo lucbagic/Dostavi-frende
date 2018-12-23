adb75209d4400c15235fc18b41c1bddc3d1e191d
Lucija-PC\Lucija Bagić <lucbagic@foi.hr>
2018-12-23 22:09:37 +0100

    Dodana donja navigacija.

--- Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/MainActivity.java
+++ Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/MainActivity.java
@@ -1,8 +1,12 @@
 package hr.dostavifrende.dostavifrende;
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,7 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new ActiveUsersFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    android.support.v4.app.Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_active_users:
+                            selectedFragment = new ActiveUsersFragment();
+                            break;
+                        case R.id.nav_offer:
+                            selectedFragment = new OfferServiceFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
+
     public void openRegister(View v){
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
