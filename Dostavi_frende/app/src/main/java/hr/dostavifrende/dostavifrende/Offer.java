386a506d98ecbeae11560e27dd63ca40341e9475
Lucija-PC\Lucija Bagić <lucbagic@foi.hr>
2019-01-23 20:43:00 +0100

    Rad na dokumentaciji.

--- Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/Offer.java
+++ Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/Offer.java
@@ -2,16 +2,44 @@ package hr.dostavifrende.dostavifrende;
 
 
 public class Offer {
-    public String korisnik, napomena, datum;
+    public String korisnik, napomena, datum, grad;
 
-    public Offer(String korisnik, String datum, String napomena)
+    public Offer(String korisnik, String grad, String napomena)
     {
         this.korisnik = korisnik;
-        this.datum = datum;
+        this.grad = grad;
         this.napomena = napomena;
     }
 
     Offer(){
 
+    }
+
+    public String getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(String korisnik) {
+        this.korisnik = korisnik;
+    }
+
+    public String getNapomena() {
+        return napomena;
+    }
+
+    public void setNapomena(String napomena) {
+        this.napomena = napomena;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public void setGrad(String grad) {
+        this.grad = grad;
     }
 }
