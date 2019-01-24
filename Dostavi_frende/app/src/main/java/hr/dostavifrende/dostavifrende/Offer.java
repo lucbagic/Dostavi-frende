807d4a8fc339300d30d442dc15c131d2fe0f94a0
Lucija-PC\Lucija Bagić <lucbagic@foi.hr>
2019-01-24 21:16:23 +0100

    Dodavanje RecyclerView s CardHolderima na fragment Aktivni i spremanje url-a od slike u korisnika.

--- Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/Offer.java
+++ Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/Offer.java
@@ -2,16 +2,49 @@ package hr.dostavifrende.dostavifrende;
 
 
 public class Offer {
-    public String korisnik, napomena, datum;
+    public String korisnik, napomena, datum, grad;
 
-    public Offer(String korisnik, String datum, String napomena)
+    public Offer(String korisnik, String datum, String napomena, String grad)
     {
         this.korisnik = korisnik;
         this.datum = datum;
         this.napomena = napomena;
+        this.grad = grad;
     }
 
-    Offer(){
+    public Offer(){
 
+    }
+
+    public String getKorisnik() {
+        return korisnik;
+    }
+
+    public void setKorisnik(String korisnik) {
+        this.korisnik = korisnik;
+    }
+
+    public String getNapomena() {
+        return napomena;
+    }
+
+    public void setNapomena(String napomena) {
+        this.napomena = napomena;
+    }
+
+    public String getDatum() {
+        return datum;
+    }
+
+    public void setDatum(String datum) {
+        this.datum = datum;
+    }
+
+    public String getGrad() {
+        return grad;
+    }
+
+    public void setGrad(String grad) {
+        this.grad = grad;
     }
 }
