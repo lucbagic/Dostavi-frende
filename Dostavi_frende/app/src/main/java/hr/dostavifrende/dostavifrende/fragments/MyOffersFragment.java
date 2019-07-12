4e563554ad60a5382ab15359e5c395a02c21449b
daralilov <daralilov@foi.hr>
2019-07-12 13:30:00 +0200

    Ispravljeni importi

--- Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/fragments/MyOffersFragment.java
+++ Dostavi_frende/app/src/main/java/hr/dostavifrende/dostavifrende/fragments/MyOffersFragment.java
@@ -1,39 +1,23 @@
 package hr.dostavifrende.dostavifrende.fragments;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.media.Image;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
-import java.util.Calendar;
-
-import hr.dostavifrende.dostavifrende.Message;
 import hr.dostavifrende.dostavifrende.Offer;
 import hr.dostavifrende.dostavifrende.R;
 
