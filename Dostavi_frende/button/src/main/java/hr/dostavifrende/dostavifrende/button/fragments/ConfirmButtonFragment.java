2b3112b985b840a31beb34e97bca2abbf21c3fe9
Lucija-PC\Lucija Bagić <lucbagic@foi.hr>
2019-08-23 20:48:34 +0200

    Dependencies.

--- Dostavi_frende/button/src/main/java/hr/dostavifrende/dostavifrende/button/fragments/ConfirmButtonFragment.java
+++ Dostavi_frende/button/src/main/java/hr/dostavifrende/dostavifrende/button/fragments/ConfirmButtonFragment.java
@@ -1,8 +1,15 @@
 package hr.dostavifrende.dostavifrende.button.fragments;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
 
 import hr.dostavifrende.dostavifrende.button.R;
 import hr.dostavifrende.dostavifrende.core.ConfirmListener;
@@ -11,6 +18,10 @@ import hr.dostavifrende.dostavifrende.core.fragments.BaseFragment;
 public class ConfirmButtonFragment extends BaseFragment implements View.OnClickListener {
 
     private ConfirmListener confirmListener;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    StorageReference storageReference;
+    DatabaseReference rootReference;
 
     @Override
     public int getLayout() {
@@ -20,6 +31,10 @@ public class ConfirmButtonFragment extends BaseFragment implements View.OnClickL
     @Override
     public void init(View view) {
         Button confirm_button = view.findViewById(R.id.confirm_button);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        rootReference = FirebaseDatabase.getInstance().getReference();
+        Log.i("LUCKA", user.getUid());
         confirm_button.setOnClickListener(this);
     }
 
